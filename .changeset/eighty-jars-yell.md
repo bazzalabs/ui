76b809b5528a6632b8fd6cd7007ec38153fbea2e
Kian Bazarjani <kian@bazza.dev>
2025-12-05 14:47:49 -0500

    updates to filter.clearall

---
+++ .changeset/eighty-jars-yell.md
@@ -0,0 +1,5 @@
+---
+"@bazza-ui/registry-filters": minor
+---
+
+Change `Filter.ClearAll` button to `outline` variant, use custom icon
--- apps/web/registry/ui/filter/components/actions/filter-actions.tsx
+++ apps/web/registry/ui/filter/components/actions/filter-actions.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { type DataTableFilterActions, type Locale, t } from '@bazza-ui/filters'
-import { FilterXIcon } from 'lucide-react'
+import { FilterXIcon, XIcon } from 'lucide-react'
 import { type ComponentPropsWithoutRef, forwardRef } from 'react'
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
@@ -28,7 +28,7 @@ export const FilterActions = forwardRef<HTMLButtonElement, FilterActionsProps>(
       actions: actionsProp,
       locale: localeProp,
       className,
-      variant = 'destructive',
+      variant = 'outline',
       ...props
     },
     ref,
@@ -44,12 +44,16 @@ export const FilterActions = forwardRef<HTMLButtonElement, FilterActionsProps>(
         ref={ref}
         data-slot="filter-actions"
         data-state={hasFilters ? 'visible' : 'hidden'}
-        className={cn('h-7 !px-2', !hasFilters && 'hidden', className)}
+        className={cn(
+          'h-7 !px-2 group/button',
+          !hasFilters && 'hidden',
+          className,
+        )}
         variant={variant}
         onClick={actions?.removeAllFilters}
         {...props}
       >
-        <FilterXIcon />
+        <ListFilterMinusIcon className="group-hover/button:text-primary text-muted-foreground" />
         <span className="hidden md:block">{t('clear', locale)}</span>
       </Button>
     )
@@ -57,6 +61,27 @@ export const FilterActions = forwardRef<HTMLButtonElement, FilterActionsProps>(
 )
 
 FilterActions.displayName = 'FilterActions'
+
+export const ListFilterMinusIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M12 5H2" />
+    <path d="M6 12H18" />
+    <path d="M9 19H15" />
+    <path d="M16.8789 2.87891L21.1215 7.12155" />
+    <path d="M16.8785 7.12155L21.1211 2.87891" />
+  </svg>
+)
 
 export namespace FilterActions {
   export type Props = FilterActionsProps
