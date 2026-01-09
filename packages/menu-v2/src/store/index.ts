5fd28e42526d63550b59d27b61a329de5fe4d6d5
Kian Bazarjani <kian@bazza.dev>
2026-01-08 19:15:11 -0500

    clean slate

--- packages/menu-v2/src/store/index.ts
+++
@@ -1,9 +0,0 @@
-export { MenuStore, ROOT_SURFACE_ID } from './menu-store.js'
-export type {
-  MenuState,
-  MenuContext,
-  RowRecord,
-  SurfaceState,
-  Direction,
-  ActivationCause,
-} from './types.js'
