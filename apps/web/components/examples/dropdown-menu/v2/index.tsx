16e650390c987cbf199dccb7de0841dbf4bbf6fa
Kian Bazarjani <kian@bazza.dev>
2026-01-10 17:40:19 -0500

    remove dropdown menu v2 stuff

--- apps/web/components/examples/dropdown-menu/v2/index.tsx
+++
@@ -1,7 +0,0 @@
-import { DropdownMenuV2_Basic } from './basic'
-import { DropdownMenuV2_Submenus } from './submenus'
-
-export const DropdownMenuV2 = {
-  Basic: DropdownMenuV2_Basic,
-  Submenus: DropdownMenuV2_Submenus,
-}
