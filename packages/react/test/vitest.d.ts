1431b6a68f898c70f8893866b671e9da7186871d
Kian Bazarjani <kian@bazza.dev>
2026-01-15 06:46:34 -0500

    update test packages

--- packages/react/test/vitest.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest/globals" />
-import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
-
-declare module 'vitest' {
-  interface Assertion<T = unknown>
-    extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
-  interface AsymmetricMatchersContaining
-    extends TestingLibraryMatchers<unknown, unknown> {}
-}
