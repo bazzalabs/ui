9c8bfbd5b2224872b33a115d15de6186cf2dbad3
Kian Bazza <kian@bazza.dev>
2026-03-29 21:39:25 -0400

    chore(dropdown-menu): change audience to preview (#282)

--- apps/web/components/sidebar/data.tsx
+++ apps/web/components/sidebar/data.tsx
@@ -148,7 +148,7 @@ export const componentItems: ComponentItem[] = [
     type: 'single',
     title: 'Dropdown Menu',
     url: '/docs/dropdown-menu',
-    audience: 'private',
+    audience: 'preview',
   },
   {
     type: 'single',
