80a4a3a849175c57023946b16365e92ee87c1c20
Kian Bazarjani <kian@bazza.dev>
2026-07-12 03:41:48 -0400

    feat(command-menu): close on backdrop pointerdown

--- packages/react/src/command-menu/backdrop/backdrop.tsx
+++ packages/react/src/command-menu/backdrop/backdrop.tsx
@@ -2,21 +2,49 @@
 
 import { Dialog } from '@base-ui/react/dialog'
 import * as React from 'react'
+import { useMaybePopupMenuContext } from '../../internal/popup-menu/contexts/popup-menu-context.js'
+import { REASONS } from '../../utils/events/index.js'
 
 export interface CommandMenuBackdropProps extends Dialog.Backdrop.Props {}
 export type CommandMenuBackdropState = Dialog.Backdrop.State
 
 /**
  * An overlay displayed beneath the command menu popup.
+ *
+ * Pressing down on the backdrop closes the menu immediately (pointerdown),
+ * matching the outside-press behavior of the other menus, instead of waiting
+ * for the Dialog's click-time dismissal. Consumers can opt out by calling
+ * `event.preventDefault()` in their own `onPointerDown`.
  */
 export const CommandMenuBackdrop = React.forwardRef<
   HTMLDivElement,
   CommandMenuBackdrop.Props
 >(function CommandMenuBackdrop(props, forwardedRef) {
+  const { onPointerDown, ...rest } = props
+  const popupMenuContext = useMaybePopupMenuContext()
+
+  const handlePointerDown = React.useCallback<
+    NonNullable<Dialog.Backdrop.Props['onPointerDown']>
+  >(
+    (event) => {
+      onPointerDown?.(event)
+
+      if (event.defaultPrevented) {
+        return
+      }
+
+      if (popupMenuContext && !popupMenuContext.disabled) {
+        popupMenuContext.closeAll(REASONS.outsidePress, event.nativeEvent)
+      }
+    },
+    [onPointerDown, popupMenuContext],
+  )
+
   return (
     <Dialog.Backdrop
       ref={forwardedRef}
-      {...props}
+      {...rest}
+      onPointerDown={handlePointerDown}
       data-command-menu-backdrop=""
     />
   )
